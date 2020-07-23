8064ba6c99dc0b3c5708a0856f150eee9baeca9c
lyn2019 <1134298165@qq.com>
2020-07-23 13:27:52 +0800

    add

---
+++ app/components/SettingMenu.css.d.ts
@@ -0,0 +1,9 @@
+declare const styles: {
+  readonly "controlIcon": string;
+  readonly "menuContent": string;
+  readonly "menuItem": string;
+  readonly "open": string;
+  readonly "settingMenuBox": string;
+};
+export = styles;
+
